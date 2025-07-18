842a2463b65b6b7b9a355aa75ea20c2958cd5805
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-07-17 22:21:17 -0400

    feat: Test and implement FiniteLinearMovingColorProviderDefinitionReader

---
+++ src/main/java/definitions/providers/FiniteLinearMovingColorProviderDefinitionReader.java
@@ -0,0 +1,33 @@
+package definitions.providers;
+
+import inaugural.soliloquy.tools.Check;
+import soliloquy.specs.io.graphics.renderables.providers.FiniteLinearMovingColorProvider;
+import soliloquy.specs.io.graphics.renderables.providers.factories.FiniteLinearMovingColorProviderFactory;
+import soliloquy.specs.ui.definitions.providers.FiniteLinearMovingColorProviderDefinition;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import static inaugural.soliloquy.tools.collections.Collections.mapOf;
+import static soliloquy.specs.common.valueobjects.Pair.pairOf;
+
+public class FiniteLinearMovingColorProviderDefinitionReader {
+    private final FiniteLinearMovingColorProviderFactory FACTORY;
+
+    public FiniteLinearMovingColorProviderDefinitionReader(
+            FiniteLinearMovingColorProviderFactory factory) {
+        FACTORY = Check.ifNull(factory, "factory");
+    }
+
+    public FiniteLinearMovingColorProvider read(
+            FiniteLinearMovingColorProviderDefinition definition, long contentRenderTimestamp) {
+        Check.ifNull(definition, "definition");
+        Check.ifNull(definition.VALUES_AT_TIMESTAMPS, "definition.VALUES_AT_TIMESTAMPS");
+        var valsAtTimestamps = mapOf(Arrays.stream(definition.VALUES_AT_TIMESTAMPS)
+                .map(val -> pairOf(
+                        Check.ifNull(val, "val within definition.VALUES_AT_TIMESTAMPS")
+                                .FIRST + contentRenderTimestamp, val.SECOND)));
+        return FACTORY.make(UUID.randomUUID(), valsAtTimestamps,
+                definition.HUE_MOVEMENT_IS_CLOCKWISE, null, null);
+    }
+}
---
+++ src/test/java/inaugural/soliloquy/ui/test/definitions/providers/FiniteLinearMovingColorProviderDefinitionReaderTests.java
@@ -0,0 +1,71 @@
+package inaugural.soliloquy.ui.test.definitions.providers;
+
+import definitions.providers.FiniteLinearMovingColorProviderDefinitionReader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import soliloquy.specs.io.graphics.renderables.providers.FiniteLinearMovingColorProvider;
+import soliloquy.specs.io.graphics.renderables.providers.factories.FiniteLinearMovingColorProviderFactory;
+
+import static inaugural.soliloquy.tools.collections.Collections.listOf;
+import static inaugural.soliloquy.tools.collections.Collections.mapOf;
+import static inaugural.soliloquy.tools.random.Random.*;
+import static inaugural.soliloquy.tools.testing.Assertions.once;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static soliloquy.specs.common.valueobjects.Pair.pairOf;
+import static soliloquy.specs.ui.definitions.providers.FiniteLinearMovingColorProviderDefinition.finiteLinearMovingColor;
+import static soliloquy.specs.ui.definitions.providers.FiniteLinearMovingProviderDefinition.finiteLinearMoving;
+
+@ExtendWith(MockitoExtension.class)
+public class FiniteLinearMovingColorProviderDefinitionReaderTests {
+    @Mock private FiniteLinearMovingColorProvider mockProvider;
+    @Mock private FiniteLinearMovingColorProviderFactory mockFactory;
+
+    private FiniteLinearMovingColorProviderDefinitionReader reader;
+
+    @BeforeEach
+    public void setUp() {
+        reader = new FiniteLinearMovingColorProviderDefinitionReader(mockFactory);
+    }
+
+    @Test
+    public void testConstructorWithInvalidArgs() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new FiniteLinearMovingColorProviderDefinitionReader(null));
+    }
+
+    @Test
+    public void testRead() {
+        when(mockFactory.make(any(), any(), any(), any(), any())).thenReturn(mockProvider);
+        var vals = pairOf(randomInt(), randomColor());
+        var isClockwise = randomBoolean();
+        var contentRenderTimestamp = randomLong();
+        var definition = finiteLinearMovingColor(listOf(isClockwise), vals);
+
+        var provider = reader.read(definition, contentRenderTimestamp);
+
+        assertSame(mockProvider, provider);
+        var expectedRenderTimestamp = contentRenderTimestamp + vals.FIRST;
+        verify(mockFactory, once()).make(any(),
+                eq(mapOf(pairOf(expectedRenderTimestamp, vals.SECOND))), eq(listOf(isClockwise)),
+                isNull(), isNull());
+    }
+
+    @Test
+    public void testReadWithInvalidArgs() {
+        assertThrows(IllegalArgumentException.class, () -> reader.read(null, randomLong()));
+        assertThrows(IllegalArgumentException.class, () -> reader.read(finiteLinearMovingColor(null,
+                        (soliloquy.specs.common.valueobjects.Pair<Integer, java.awt.Color>) null),
+                randomLong()));
+        assertThrows(IllegalArgumentException.class, () -> reader.read(finiteLinearMovingColor(null,
+                        (soliloquy.specs.common.valueobjects.Pair<Integer, java.awt.Color>[]) null),
+                randomLong()));
+    }
+}

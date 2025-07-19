6232fae009349744e3ed7436b7b1337d4bf00a73
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-07-18 23:07:57 -0400

    feat: Test and implement RasterizedLineSegmentRenderableDefinitionReader

---
+++ src/main/java/inaugural/soliloquy/ui/definitions/RasterizedLineSegmentRenderableDefinitionReader.java
@@ -0,0 +1,45 @@
+package inaugural.soliloquy.ui.definitions;
+
+import inaugural.soliloquy.tools.Check;
+import inaugural.soliloquy.ui.definitions.providers.ProviderDefinitionReader;
+import soliloquy.specs.common.valueobjects.Vertex;
+import soliloquy.specs.io.graphics.renderables.RasterizedLineSegmentRenderable;
+import soliloquy.specs.io.graphics.renderables.factories.RasterizedLineSegmentRenderableFactory;
+import soliloquy.specs.io.graphics.renderables.providers.ProviderAtTime;
+import soliloquy.specs.io.graphics.rendering.RenderableStack;
+import soliloquy.specs.ui.definitions.content.RasterizedLineSegmentRenderableDefinition;
+
+import java.util.UUID;
+
+public class RasterizedLineSegmentRenderableDefinitionReader {
+    private final RasterizedLineSegmentRenderableFactory FACTORY;
+    private final ProviderDefinitionReader PROVIDER_READER;
+    private final short DEFAULT_STIPPLE_PATTERN;
+    private final short DEFAULT_STIPPLE_FACTOR;
+
+    public RasterizedLineSegmentRenderableDefinitionReader(
+            RasterizedLineSegmentRenderableFactory factory,
+            ProviderDefinitionReader providerReader,
+            short defaultStipplePattern,
+            short defaultStippleFactor) {
+        FACTORY = Check.ifNull(factory, "factory");
+        PROVIDER_READER = Check.ifNull(providerReader, "providerReader");
+        DEFAULT_STIPPLE_PATTERN = defaultStipplePattern;
+        DEFAULT_STIPPLE_FACTOR = defaultStippleFactor;
+    }
+
+    public RasterizedLineSegmentRenderable read(RenderableStack stack,
+                                                RasterizedLineSegmentRenderableDefinition definition) {
+        var vertex1 = PROVIDER_READER.read(definition.VERTEX_1_PROVIDER);
+        var vertex2 = PROVIDER_READER.read(definition.VERTEX_2_PROVIDER);
+        var thickness = PROVIDER_READER.read(definition.THICKNESS_PROVIDER);
+        short stipplePattern = definition.stipplePattern == null ? DEFAULT_STIPPLE_PATTERN :
+                definition.stipplePattern;
+        short stippleFactor = definition.stippleFactor == null ? DEFAULT_STIPPLE_FACTOR :
+                definition.stippleFactor;
+        var color = PROVIDER_READER.read(definition.COLOR_PROVIDER);
+
+        return FACTORY.make(vertex1, vertex2, thickness, stipplePattern, stippleFactor, color,
+                definition.Z, UUID.randomUUID(), stack);
+    }
+}
---
+++ src/test/java/inaugural/soliloquy/ui/test/definitions/RasterizedLineSegmentRenderableDefinitionReaderTests.java
@@ -0,0 +1,158 @@
+package inaugural.soliloquy.ui.test.definitions;
+
+import inaugural.soliloquy.ui.definitions.RasterizedLineSegmentRenderableDefinitionReader;
+import inaugural.soliloquy.ui.definitions.providers.ProviderDefinitionReader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import soliloquy.specs.common.valueobjects.Vertex;
+import soliloquy.specs.io.graphics.renderables.RasterizedLineSegmentRenderable;
+import soliloquy.specs.io.graphics.renderables.factories.RasterizedLineSegmentRenderableFactory;
+import soliloquy.specs.io.graphics.renderables.providers.ProviderAtTime;
+import soliloquy.specs.io.graphics.rendering.RenderableStack;
+import soliloquy.specs.ui.definitions.providers.AbstractProviderDefinition;
+
+import java.awt.*;
+
+import static inaugural.soliloquy.tools.random.Random.randomInt;
+import static inaugural.soliloquy.tools.random.Random.randomShort;
+import static inaugural.soliloquy.tools.testing.Assertions.once;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static soliloquy.specs.ui.definitions.content.RasterizedLineSegmentRenderableDefinition.rasterizedLineSegment;
+
+@ExtendWith(MockitoExtension.class)
+public class RasterizedLineSegmentRenderableDefinitionReaderTests {
+    private final short STIPPLE_PATTERN = randomShort();
+    private final short STIPPLE_FACTOR = randomShort();
+    private final short DEFAULT_STIPPLE_PATTERN = randomShort();
+    private final short DEFAULT_STIPPLE_FACTOR = randomShort();
+    private final int Z = randomInt();
+
+    @Mock private RasterizedLineSegmentRenderable mockRenderable;
+    @Mock private RasterizedLineSegmentRenderableFactory mockFactory;
+    @Mock private ProviderDefinitionReader mockProviderDefinitionReader;
+    @Mock private RenderableStack mockStack;
+
+    private RasterizedLineSegmentRenderableDefinitionReader reader;
+
+    @BeforeEach
+    public void setUp() {
+        reader = new RasterizedLineSegmentRenderableDefinitionReader(mockFactory,
+                mockProviderDefinitionReader, DEFAULT_STIPPLE_PATTERN, DEFAULT_STIPPLE_FACTOR);
+    }
+
+    @Test
+    public void testConstructorWithInvalidArgs() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new RasterizedLineSegmentRenderableDefinitionReader(null,
+                        mockProviderDefinitionReader, DEFAULT_STIPPLE_PATTERN,
+                        DEFAULT_STIPPLE_FACTOR));
+        assertThrows(IllegalArgumentException.class,
+                () -> new RasterizedLineSegmentRenderableDefinitionReader(mockFactory, null,
+                        DEFAULT_STIPPLE_PATTERN, DEFAULT_STIPPLE_FACTOR));
+    }
+
+    @Test
+    public void testRead() {
+        @SuppressWarnings("unchecked") var vertex1Definition =
+                (AbstractProviderDefinition<Vertex>) mock(AbstractProviderDefinition.class);
+        @SuppressWarnings("unchecked") var vertex2Definition =
+                (AbstractProviderDefinition<Vertex>) mock(AbstractProviderDefinition.class);
+        @SuppressWarnings("unchecked") var thicknessDefinition =
+                (AbstractProviderDefinition<Float>) mock(AbstractProviderDefinition.class);
+        @SuppressWarnings("unchecked") var colorDefinition =
+                (AbstractProviderDefinition<Color>) mock(AbstractProviderDefinition.class);
+
+        @SuppressWarnings("unchecked") var vertex1 =
+                (ProviderAtTime<Vertex>) mock(ProviderAtTime.class);
+        @SuppressWarnings("unchecked") var vertex2 =
+                (ProviderAtTime<Vertex>) mock(ProviderAtTime.class);
+        @SuppressWarnings("unchecked") var thickness =
+                (ProviderAtTime<Float>) mock(ProviderAtTime.class);
+        @SuppressWarnings("unchecked") var color =
+                (ProviderAtTime<Color>) mock(ProviderAtTime.class);
+
+        when(mockProviderDefinitionReader.read(vertex1Definition)).thenReturn(vertex1);
+        when(mockProviderDefinitionReader.read(vertex2Definition)).thenReturn(vertex2);
+        when(mockProviderDefinitionReader.read(thicknessDefinition)).thenReturn(thickness);
+        when(mockProviderDefinitionReader.read(colorDefinition)).thenReturn(color);
+
+        when(mockFactory.make(any(), any(), any(), anyShort(), anyShort(), any(), anyInt(), any(),
+                any())).thenReturn(mockRenderable);
+
+        var definition =
+                rasterizedLineSegment(vertex1Definition, vertex2Definition, thicknessDefinition,
+                        colorDefinition, Z)
+                        .withStipple(STIPPLE_PATTERN, STIPPLE_FACTOR);
+
+        var renderable = reader.read(mockStack, definition);
+
+        assertNotNull(renderable);
+        assertSame(mockRenderable, renderable);
+        verify(mockProviderDefinitionReader, once()).read(vertex1Definition);
+        verify(mockProviderDefinitionReader, once()).read(vertex2Definition);
+        verify(mockProviderDefinitionReader, once()).read(thicknessDefinition);
+        verify(mockProviderDefinitionReader, once()).read(colorDefinition);
+        verify(mockFactory, once()).make(
+                same(vertex1), same(vertex2),
+                same(thickness), eq(STIPPLE_PATTERN), eq(STIPPLE_FACTOR),
+                same(color),
+                eq(Z),
+                isNotNull(),
+                same(mockStack)
+        );
+    }
+
+    @Test
+    public void testReadWithMinimalArgs() {
+        @SuppressWarnings("unchecked") var vertex1Definition =
+                (AbstractProviderDefinition<Vertex>) mock(AbstractProviderDefinition.class);
+        @SuppressWarnings("unchecked") var vertex2Definition =
+                (AbstractProviderDefinition<Vertex>) mock(AbstractProviderDefinition.class);
+        @SuppressWarnings("unchecked") var thicknessDefinition =
+                (AbstractProviderDefinition<Float>) mock(AbstractProviderDefinition.class);
+        @SuppressWarnings("unchecked") var colorDefinition =
+                (AbstractProviderDefinition<Color>) mock(AbstractProviderDefinition.class);
+
+        @SuppressWarnings("unchecked") var vertex1 =
+                (ProviderAtTime<Vertex>) mock(ProviderAtTime.class);
+        @SuppressWarnings("unchecked") var vertex2 =
+                (ProviderAtTime<Vertex>) mock(ProviderAtTime.class);
+        @SuppressWarnings("unchecked") var thickness =
+                (ProviderAtTime<Float>) mock(ProviderAtTime.class);
+        @SuppressWarnings("unchecked") var color =
+                (ProviderAtTime<Color>) mock(ProviderAtTime.class);
+
+        when(mockProviderDefinitionReader.read(vertex1Definition)).thenReturn(vertex1);
+        when(mockProviderDefinitionReader.read(vertex2Definition)).thenReturn(vertex2);
+        when(mockProviderDefinitionReader.read(thicknessDefinition)).thenReturn(thickness);
+        when(mockProviderDefinitionReader.read(colorDefinition)).thenReturn(color);
+
+        when(mockFactory.make(any(), any(), any(), anyShort(), anyShort(), any(), anyInt(), any(),
+                any())).thenReturn(mockRenderable);
+
+        var definition =
+                rasterizedLineSegment(vertex1Definition, vertex2Definition, thicknessDefinition,
+                        colorDefinition, Z);
+
+        var renderable = reader.read(mockStack, definition);
+
+        assertNotNull(renderable);
+        assertSame(mockRenderable, renderable);
+        verify(mockProviderDefinitionReader, once()).read(vertex1Definition);
+        verify(mockProviderDefinitionReader, once()).read(vertex2Definition);
+        verify(mockProviderDefinitionReader, once()).read(thicknessDefinition);
+        verify(mockProviderDefinitionReader, once()).read(colorDefinition);
+        verify(mockFactory, once()).make(
+                same(vertex1), same(vertex2),
+                same(thickness), eq(DEFAULT_STIPPLE_PATTERN), eq(DEFAULT_STIPPLE_FACTOR),
+                same(color),
+                eq(Z),
+                isNotNull(),
+                same(mockStack)
+        );
+    }
+}
